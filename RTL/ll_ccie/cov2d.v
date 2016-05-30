e16ef69fbaf00ce6c5eab4d0c6f917626672fb8c
Peng Qu <shen_yhx@163.com>
2016-05-30 11:19:42 -0700

    matrix multiplier pipeline

---
+++ RTL/ll_ccie/cov2d.v
@@ -0,0 +1,12 @@
+module conv2d
+	(
+		input [511:0] data;
+		input [511:0] kernel;
+
+		output [31:0] res;
+	);
+
+
+reg [511:0] buffer;
+
+endmodule
---
+++ RTL/ll_ccie/max_polling.v
@@ -0,0 +1,23 @@
+module max_pooling (
+	input [127: 0] in;
+	output [31:0] out;
+);
+
+wire signed [31:0] in0;
+wire signed [31:0] in1;
+wire signed [31:0] in2;
+wire signed [31:0] in3;
+
+assign in0 = in[31:0];
+assign in1 = in[63:32];
+assign in2 = in[95:64];
+assign in3 = in[127:96];
+
+reg signed [31:0] max0;
+reg signed [31:0] max1;
+
+assign max0 = in0>in1?in0:in1;
+assign max1 = in2>in3?in2:in3;
+
+assign out = max0>max1?max0:max1;
+
